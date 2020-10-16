29daa4ba02e4b86231eeef2e350c798d21a54024
Daniel Moll <daniel@perfana.io>
2020-10-16 20:36:02 +0200

    updated database endpoint

--- src/test/scala/io/perfana/afterburner/gatling/useCases/Database.scala
+++ src/test/scala/io/perfana/afterburner/gatling/useCases/Database.scala
@@ -9,7 +9,7 @@ import scala.concurrent.duration._
 object Database {
 
   val call = exec(http("database_call")
-            .get("/remote/call-many?count=1&path=/db/employee/name?firstName=${FIRST_NAME}")
+            .get("/remote/call-many?count=1&path=/db/employee/find-by-name?lastName=${LAST_NAME}")
             .header("perfana-request-name", "database_call")
             .header("perfana-test-run-id", "${testRunId}")
             .check(status.is(200)))
