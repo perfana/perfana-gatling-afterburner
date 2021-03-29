199782c9031661e5ca084f6c1f4745632fc90e36
Daniel Moll <daniel@perfana.io>
2021-03-29 21:45:14 +0200

    changed delay

--- src/test/scala/io/perfana/afterburner/gatling/useCases/FlakyCall.scala
+++ src/test/scala/io/perfana/afterburner/gatling/useCases/FlakyCall.scala
@@ -6,7 +6,7 @@ import io.gatling.http.Predef._
 object FlakyCall {
 
   val call = exec(http("flaky_call")
-            .get("/flaky?maxRandomDelay=2000&flakiness=25")
+            .get("/flaky?maxRandomDelay=500&flakiness=25")
             .header("perfana-request-name", "flaky_call")
             .header("perfana-test-run-id", "${testRunId}")
             .check(status.is(200))
